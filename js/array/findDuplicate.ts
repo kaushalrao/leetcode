a0fb98b20d26e4db2bfc9946fd68edddf9ea253f
Kaushal <kaushalraok@gmail.com>
2024-11-04 07:25:09 +0530

    Add find duplicate code

---
+++ js/array/findDuplicate.ts
@@ -0,0 +1,21 @@
+const findDuplicates = (nums:number[]) => {
+    let slow = 0;
+    let fast = 0;
+
+    // Phase 1: Find intersection point of Tortoise and Hare
+    do {
+        slow = nums[slow];
+        fast = nums[nums[fast]];
+    } while (slow !== fast);
+
+    // Phase 2: Find entrance to the cycle
+    let slow2 = 0;
+    while (slow !== slow2) {
+        slow = nums[slow];
+        slow2 = nums[slow2];
+    }
+
+    return slow;
+}
+
+console.log(findDuplicates([1,2,3,3,1]));
